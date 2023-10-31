6fed1fec0f40895735dee0b6693509ea04b50a05
Haix1xx <haixinhtuoi123@gmail.com>
2023-10-31 22:40:22 +0700

    fix profile popluate when get post

--- beegin-app-server/models/userModel.js
+++ beegin-app-server/models/userModel.js
@@ -3,62 +3,75 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  // name: {
-  //   type: String,
-  //   required: [true, "Please tell us your name!"],
-  // },
-  email: {
-    type: String,
-    required: [true, "Please provide your email"],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"],
-  },
-  phonenumber: String,
-  role: {
-    type: String,
-    enum: ["user", "business", "admin"],
-    default: "user",
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    minlength: 8,
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    validate: {
-      // Use an arrow function to capture the correct context of 'this'
-      validator: function (el) {
-        return el === this.password;
+const userSchema = new mongoose.Schema(
+  {
+    // name: {
+    //   type: String,
+    //   required: [true, "Please tell us your name!"],
+    // },
+    email: {
+      type: String,
+      required: [true, "Please provide your email"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    phonenumber: String,
+    role: {
+      type: String,
+      enum: ["user", "business", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        // Use an arrow function to capture the correct context of 'this'
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords are not the same!",
       },
-      message: "Passwords are not the same!",
+    },
+    passwordChangedAt: Date,
+    preferences: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category", // Reference to the Category model
+      },
+    ],
+    // profile: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Profile", // Reference to the Profile model
+    // },
+    refreshToken: String,
+    verifyToken: String,
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  passwordChangedAt: Date,
-  preferences: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category", // Reference to the Category model
-    },
-  ],
-  profile: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Profile", // Reference to the Profile model
-  },
-  refreshToken: String,
-  verifyToken: String,
-  verify: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+userSchema.virtual("profile", {
+  ref: "Profile",
+  foreignField: "user",
+  localField: "_id",
+  justOne: true,
 });
 
 userSchema.pre("save", async function (next) {
