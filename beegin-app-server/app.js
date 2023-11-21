64f4022479d651e2ec6dc988bc9e0d477760ca39
Haix1xx <haixinhtuoi123@gmail.com>
2023-11-22 02:14:51 +0700

    fix

--- beegin-app-server/app.js
+++ beegin-app-server/app.js
@@ -28,6 +28,7 @@ const cors = require("cors");
 const app = express();
 const upload = multer();
 
+app.use(express.json({ limit: "10kb" }));
 app.use(
   cors({
     // origin: ["http://localhost:3000", "https://beegin-app.vercel.app"], // Replace with the origin of your client application
@@ -73,7 +74,7 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
+// app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 app.use(bodyParser());
