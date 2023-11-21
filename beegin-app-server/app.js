1bf3672b6a6c217082fc43a9c8bdfe8d45c7c1b0
Haix1xx <haixinhtuoi123@gmail.com>
2023-11-22 02:22:27 +0700

    fix

--- beegin-app-server/app.js
+++ beegin-app-server/app.js
@@ -36,7 +36,7 @@ app.use(
     credentials: true, // Allow credentials (cookies) to be sent
   })
 );
-
+app.use(cookieParser());
 app.use(
   session({
     resave: false,
@@ -45,10 +45,23 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60,
       sameSite: "none",
+      domain: "https://beegin-app.vercel.app",
       secure: true,
     },
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+
 app.set("trust proxy", 1);
 // app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
@@ -76,7 +89,7 @@ app.use("/api", limiter);
 // Body parser, reading data from body into req.body
 // app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-app.use(cookieParser());
+
 app.use(bodyParser());
 app.use(upload.any());
 app.use(express.static("public"));
