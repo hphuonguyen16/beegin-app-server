c8c0e93eb3198d5103719863d15d45ea268077d8
Haix1xx <haixinhtuoi123@gmail.com>
2023-12-16 15:44:50 +0700

    review notification model

--- beegin-app-server/controllers/notificationController.js
+++
@@ -1,9 +0,0 @@
-const catchAsync = require('./../utils/catchAsync');
-const notificationService = require('../services/notificationServices');
-
-exports.getAllNotification = catchAsync(async (req, res) => {
-    const data = await notificationService.getAllNotification(req.user.id);
-    return res.status(200).json(
-        data
-    );
-});
--- beegin-app-server/models/notificationModel.js
+++ beegin-app-server/models/notificationModel.js
@@ -1,14 +1,32 @@
 const mongoose = require("mongoose");
 
 const Notificationschema = new mongoose.Schema({
-  user: {
+  receiver: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Reference to the User model
-    required: [true, 'A notification must belong to a user'],
+    ref: "User", // Reference to the User model
+    required: [true, "A notification must belong to a user"],
   },
-  content: {
+  type: {
     type: String,
-    required: [true, 'Please provide a notification content'],
+    enum: [
+      "follow",
+      "comment",
+      "like post",
+      "like comment",
+      "reply comment",
+      "message",
+    ],
+    required: [true, "Notification type must be declared"],
+  },
+  actors: [
+    {
+      type: String,
+    },
+  ],
+  image: String,
+  contentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: [true, "A notification must contain content id"],
   },
   read: {
     type: Boolean,
@@ -18,8 +36,12 @@ const Notificationschema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const NotificationModel = mongoose.model('Notification', Notificationschema);
+const NotificationModel = mongoose.model("Notification", Notificationschema);
 
 module.exports = NotificationModel;
