13b646185c17131a414db68d2ed967fae23395e9
vnhxn <vinhnhan576@gmail.com>
2023-12-19 00:32:08 +0700

    get only active posts

--- beegin-app-server/services/postServices.js
+++ beegin-app-server/services/postServices.js
@@ -164,7 +164,7 @@ exports.getPostById = (id) => {
 exports.getPostsByMe = (userId) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const posts = await Post.find({ user: userId }).sort({ createdAt: -1 });
+      const posts = await Post.find({ user: userId, isActived: true }).sort({ createdAt: -1 });
       resolve({
         status: "success",
         results: posts.length,
